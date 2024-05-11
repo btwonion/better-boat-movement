be700cd59250bcea12cf7aa653c30e9d558d0ae4
btwonion <btwonion@gmx.de>
2024-05-12 00:08:14 +0200

    add runActive task

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,12 @@ loom {
             ideConfigGenerated(true)
             runDir("../../run")
         }
+
+        rootProject.tasks.register("runActive") {
+            group = "mod"
+
+            dependsOn(tasks.named("runClient"))
+        }
     }
 
     mixin { useLegacyMixinAp = false }
