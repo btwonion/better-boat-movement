c61024de541177417057310e9bfaafc7f89d271c
btwonion <btwonion@gmx.de>
2024-05-12 21:50:57 +0200

    use kotlin coding conventions

--- build.gradle.kts
+++ build.gradle.kts
@@ -143,7 +143,6 @@ publishMods {
     discord {
         webhookUrl = providers.environmentVariable("DISCORD_WEBHOOK")
         username = "Release Notifier"
-        avatarUrl = "https://www.svgrepo.com/show/521999/bell.svg"
         content = "# A new version of Better Boat Movement released!\n$changelogText"
     }
 }
